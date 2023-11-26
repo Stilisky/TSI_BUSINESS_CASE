6a430ffd8ce12ba7fbd587862924cf87acbb90a5
stilisky <setonde.hounnouvi@epitech.eu>
2023-11-26 15:52:50 +0100

    player route added

---
+++ SERVER/routes/playerRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+var router = express.Router()
+const {
+   createPlayer,
+   deletePlayer,
+   getPlayer,
+   getPlayers,
+   updatePlayer
+} = require('../controllers/playerController')
+const {authAdmin} = require('../middlewares/authMiddleware')
+router.route('/players').get(getPlayers).post(authAdmin, createPlayer)
+router.route('/players/:playerId').get(getPlayer).put(authAdmin, updatePlayer).delete(authAdmin, deletePlayer)
+
+module.exports = {router}
