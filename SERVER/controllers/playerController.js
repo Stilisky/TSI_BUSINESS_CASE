de90f7611f0406b075052e8208348311ae259ca1
stilisky <setonde.hounnouvi@epitech.eu>
2023-11-26 09:22:58 +0100

    Player controller update

--- SERVER/controllers/playerController.js
+++ SERVER/controllers/playerController.js
@@ -5,27 +5,43 @@ const getPlayers = async (req, res) => {
       const players = await playerService.getPlayers()
       res.status(200).json(players)
    } catch (error) {
-      res.status(500).json({'message': error})
+      res.status(400).json({'message': error})
    }
 }
 
 const getPlayer = async (req, res) => {
    try {
-      const id = req.params.playerid
+      const id = req.params.playerId
       const player = await playerService.getPlayer(id)
       res.status(200).json(player)
    } catch (error) {
-      res.status(500).json({'message': error})
+      res.status(400).json({'message': error})
    }
 }
 
 const createPlayer = async (req, res) => {
    try {
-      var performance;
-      if(req.body.performance) {
-         
-      }
+      const player = await playerService.createPlayer(req.body)
+      res.status(201).json(player)
    } catch (error) {
-      res.status(500).json({'message': error})
+      res.status(400).json({'message': error})
+   }
+}
+
+const updatePlayer = async (req, res) => {
+   try {
+      const player = await playerService.updatePlayer(req.params.playerId, req.body)
+      res.status(201).json(player)
+   } catch (error) {
+      res.status(400).json({'message': error})
+   }
+}
+
+const deletePlayer = async (req, res) => {
+   try {
+      const player = await playerService.deletePlayer(req.params.playerId)
+      res.status(201).json(player)
+   } catch (error) {
+      res.status(400).json({'message': error})
    }
 }
