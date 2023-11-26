1198bc834405951405aa19b3fcf19cd53ebf7b39
stilisky <setonde.hounnouvi@epitech.eu>
2023-11-26 23:55:39 +0100

    Middleware update

--- SERVER/middlewares/authMiddleware.js
+++ SERVER/middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ const authAdmin = async (req, res, next) => {
       let token = req.headers.authorization
       if (token) {
          token = token.slice(7, token.length)
-         jwt.verify(token,SECRET_KEY, (err, decodedToken) => {
+         jwt.verify(token,process.env.SECRET_KEY, (err, decodedToken) => {
          if(err) {
             return res.status(401).json('Invalid token')
          }
