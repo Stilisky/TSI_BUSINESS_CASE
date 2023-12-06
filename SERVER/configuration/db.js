f4ad13579c5668f0c669c54600bae1d1dc4b5bdf
stilisky <setonde.hounnouvi@epitech.eu>
2023-12-06 08:01:39 +0100

    Mongo url updated

--- SERVER/configuration/db.js
+++ SERVER/configuration/db.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 
 const connectDB = async () => {
   try {
-    const MONGO_URI = "mongodb+srv://TsiMongoDbUser:0pbVu85cgRHk6SlD@cluster0.exebe2n.mongodb.net/";
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
